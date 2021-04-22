f2cabe30ffbd87d9e801e24f12eb33455efeeb20
Kadphol <kadphol.len@outlook.co.th>
2021-04-22 23:23:02 +0700

    Digest Cycle

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 (function () {
   'use strict';
-  angular.module('NameCalculator', [])
-  .controller('NameCalculatorController', function ($scope) {
-    $scope.name = "";
-    $scope.totalValue = 0;
-
-    $scope.displayNumeric = function() {
-      var total = calculateNumbericForString($scope.name);
-      $scope.totalValue = total;
+  angular.module('CounterApp', [])
+  .controller('CounterController', CounterController);
+  
+  CounterController.$inject = ['$scope'];
+  function CounterController($scope) {
+    $scope.onceCounter = 0;
+    $scope.counter = 0;
+    $scope.name = "hello";
+    $scope.showNumberOfWatchers = function () {
+      console.log("# of Watchers: ", $scope.$$watchersCount); 
     };
 
-    function calculateNumbericForString(string) {
-      var totalString = 0;
-      for(var i = 0; i < string.length; i++) {
-        totalString += string.charCodeAt(i);
-      }
-      return totalString;
+    $scope.countOnce = function () {
+      $scope.onceCounter = 1;
     };
-  });
+
+    $scope.upCounter = function () {
+      $scope.counter++;
+    };
+
+    $scope.$watch(function () {
+      console.log("Digest loop fired!");
+    });
+    // $scope.$watch('onceCounter', function (newValue, oldValue) {
+    //   console.log("onceCounter old value: ", oldValue);
+    //   console.log("onceCounter new value: ", newValue);
+    // });
+    // $scope.$watch('counter', function (newValue, oldValue) {
+    //   console.log("counter old value: ", oldValue);
+    //   console.log("counter new value: ", newValue);
+    // });
+  };
 })();
