fc0aa1a3e600d256568224b219fd456971879f9d
Kadphol <kadphol.len@outlook.co.th>
2021-04-23 16:02:15 +0700

    initial assignment 2

---
+++ assignment2/app.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController);
+
+  function ToBuyController($scope) {
+
+  };
+
+  function AlreadyBoughtController($scope) {
+
+  };
+
+})();
