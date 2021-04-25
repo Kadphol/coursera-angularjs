d1fa3bccbfef895e4ef8b54bbdc6f94d945a4466
Kadphol <kadphol.len@outlook.co.th>
2021-04-25 15:52:20 +0700

    Assignment2

--- assignment2/app.js
+++ assignment2/app.js
@@ -2,14 +2,64 @@
   'use strict';
   angular.module('ShoppingListCheckOff', [])
   .controller('ToBuyController', ToBuyController)
-  .controller('AlreadyBoughtController', AlreadyBoughtController);
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  function ToBuyController($scope) {
-
+  var List = [
+    {
+      name: "cookies",
+      quantity: 10
+    },
+    {
+      name: "milk",
+      quantity: 1
+    },
+    {
+      name: "sodas",
+      quantity: 22
+    },
+    {
+      name: "salad",
+      quantity: 3
+    },
+    {
+      name: "cigars",
+      quantity: 12
+    }
+  ];
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var toBuy = this;
+    toBuy.list = ShoppingListCheckOffService.getShoppingList();
+    toBuy.boughtItem = function (itemIndex) {
+      ShoppingListCheckOffService.boughtItem(itemIndex);
+    }
   };
 
-  function AlreadyBoughtController($scope) {
-
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var bought = this;
+    bought.list = ShoppingListCheckOffService.getBoughtList();
   };
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var shoppingList = List;
+    var boughtList = [];
+    
+    service.boughtItem = function(itemIndex) {
+      var bought = shoppingList.splice(itemIndex, 1);
+      boughtList.push(bought);
+    };
+
+    service.getShoppingList = function() {
+      return shoppingList;
+    };
+
+    service.getBoughtList = function() {
+      return boughtList;
+    };
+  }
 
 })();
