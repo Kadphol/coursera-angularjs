765d116479820ec95741c6b98444de1d0ad90d67
Kadphol <kadphol.len@outlook.co.th>
2021-05-03 13:32:57 +0700

    Assignment 3

--- assignment3/app.js
+++ assignment3/app.js
@@ -1,14 +1,65 @@
 (function () {
+  'use strict';
   angular.module('NarrowItDownApp', [])
   .controller('NarrowItDownController', NarrowItDownController)
-  .service('MenuSearchService', MenuSearchService);
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', foundItems);
+
+  function foundItems() {
+    var ddo = {
+      templateUrl: 'foundItem.html',
+      scope: {
+        items: '<',
+        onRemove: '&'
+      }
+    };
+    return ddo;
+  }
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService) {
+    var search = this;
+    search.found = [];
+    search.getMatchedMenuItems = function() {
+      search.found = [];
+      if(search.searchTerm) {
+        var promise = MenuSearchService.getMatchedMenuItems(search.searchTerm);
+        promise.then(function(response) {
+          console.log(response)
+          search.found = response;
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+      }
+    };
 
-  }
+    search.removeItem = function (index) {
+      search.found.splice(index, 1);
+      if(search.found.length == 0) {
+        search.error = "Nothing found!"
+      }
+    };
+  };
 
-  function MenuSearchService() {
+  MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http) {
+    var service = this;
+    service.getMatchedMenuItems = function(searchTerm) {
+      return $http({
+        method: 'GET',
+        url: ('https://davids-restaurant.herokuapp.com/menu_items.json')
+      }).then(function (result) {
+        var foundItems = [];
+        var items = result.data.menu_items;
 
+        for(var i = 0; i < items.length; i++) {
+          if(items[i].description.indexOf(searchTerm) !== -1) {
+            foundItems.push(items[i]);
+          }
+        }
+        return foundItems;
+      });
+    }
   };
 })();
